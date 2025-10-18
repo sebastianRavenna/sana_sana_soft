7f3f878515cf17d5df60cca1675b34c8e0e9f66d
julietaR29 <julieta.rodriguezdg@gmail.com>
2025-10-18 17:21:18 +0200

    Persona

--- Persona.cpp
+++ Persona.cpp
@@ -1,9 +1,24 @@
+#include <iostream>
 #include <cstring>
 #include "Persona.h"
 
+using namespace std;
+
+std::string cargarCadena(){
+std::string texto;
+
+if (std::cin.peek()== '\n'){
+    std::cin.ignore();
+}
+
+std::getline(std::cin,texto);
+
+return texto;
+
+}
 Persona::Persona()
 {
-    idPersona = 0;
+    _idPersona = 0;
     strcpy(_DNI, "00000000");
     strcpy(_nombre, "SinNombre");
     strcpy(_apellido, "SinApellido");
@@ -13,40 +28,165 @@ Persona::Persona()
     strcpy(_sexo, "N");
     fechaNac = Fecha();
     fechaIngreso = Fecha();
-    estado = true;
+    _estado = true;
 }
 
 
-        void cargar();
-        void mostrar();
-        bool escribir();
-        bool leer();
-        int getIdPersona();
-        std::string getDNI()const;
-        std::string getNombre()const;
-        std::string getApellido()const;
-        std::string getDomicilio()const;
-        std::string getMail()const;
-        std::string getTelefono()const;
-        std::string getSexo()const;
-        std::string getNombreCompleto()const;
-        Fecha getFechaNac();
-        Fecha getFechaIngreso();
-        bool getEstado();
-        int getEdad();
-        int getAntiguedad();
-        void setDNI(std::string dni);
-        void setNombre(std::string nombre);
-        void setApellido(std::string apellido);
-        void setDomicilio(std::string domicilio);
-        void setMail(std::string mail);
-        void setTelefono(std::string telefono);
-        void setSexo(std::string sexo);
-        void setFechaNac (Fecha);
-        void setFechaIngreso(Fecha);
-        void setEstado(bool);
-Persona::~Persona()
-{
-    //dtor
+void Persona::cargar(){
+cout << "ID de persona: " << endl;
+cin >> _idPersona;
+cin.ignore();
+
+cout << "DNI: " << endl;
+setDNI(cargarCadena());
+cout << endl;
+
+cout << "Nombre: " << endl;
+setNombre(cargarCadena());
+cout << endl;
+
+cout << "Apellido: " << endl;
+setApellido(cargarCadena());
+cout << endl;
+
+
+cout << "Domicilio: " << endl;
+setDomicilio(cargarCadena());
+cout << endl;
+
+
+cout << "Mail: " << endl;
+setMail(cargarCadena());
+cout << endl;
+
+cout << "Telefono: " << endl;
+setTelefono(cargarCadena());
+cout << endl;
+
+cout << "Sexo (M/F): " << endl;
+setSexo(cargarCadena());
+cout << endl;
+
+cout << "Fecha de nacimiento: " << endl;
+fechaNac.Cargar();
+cout << endl;
+
+cout << "Fecha de Ingreso: " << endl;
+fechaIngreso.Cargar();
+cout << endl;
+
+_estado=true;
+
 }
+
+
+
+void Persona::mostrar() {
+    cout << "ID: " << getIdPersona() << endl;
+    cout << "DNI: " << getDNI() << endl;
+    cout << "Nombre: " << getNombre() << endl;
+    cout << "Apellido: " << getApellido() << endl;
+    cout << "Domicilio: " << getDomicilio() << endl;
+    cout << "Mail: " << getMail() << endl;
+    cout << "Telefono: " << getTelefono() << endl;
+    cout << "Sexo: " << getSexo() << endl;
+
+    cout << "Fecha de nacimiento: ";
+    fechaNac.Mostrar();
+    cout << "Fecha de ingreso: ";
+    fechaIngreso.Mostrar();
+
+    cout << "Estado: " << (_estado ? "Activo" : "Inactivo") << endl;
+}
+
+/*
+bool Persona::escribir(){
+}
+bool Persona::leer(){
+}
+*/
+int Persona::getIdPersona(){
+ return _idPersona;
+}
+string Persona::getDNI()const{
+    return string (_DNI);
+}
+string Persona::getNombre()const{
+    return string (_nombre);
+}
+string Persona::getApellido()const{
+    return string (_apellido);
+}
+string Persona::getDomicilio()const{
+    return string (_domicilio);
+}
+string Persona::getMail()const{
+    return string (_mail);
+}
+string Persona::getTelefono()const{
+    return string (_telefono);
+}
+string Persona::getSexo()const{
+    return string (_sexo);
+}
+/*
+string Persona::getNombreCompleto()const{
+
+}
+*/
+Fecha Persona::getFechaNac(){
+    return fechaNac;
+}
+Fecha Persona::getFechaIngreso(){
+    return fechaIngreso;
+}
+bool Persona::getEstado(){
+    return _estado;
+}
+/*
+int Persona::getEdad(){
+    return _edad;
+}
+*/
+
+void Persona::setDNI(const string &dni){
+strncpy(_DNI, dni.c_str(), sizeof(_DNI) - 1);
+_DNI[sizeof(_DNI) - 1] = '\0';
+}
+void Persona::setNombre(const string &nombre){
+    strncpy(_nombre,nombre.c_str(),sizeof(_nombre)-1);
+    _nombre[sizeof(_nombre)-1]= '\0';
+}
+void Persona::setApellido(const string &apellido){
+    strncpy(_apellido,apellido.c_str(),sizeof(_apellido)-1);
+    _apellido[sizeof(_apellido)-1]= '\0';
+}
+void Persona::setDomicilio(const string &domicilio){
+    strncpy(_domicilio,domicilio.c_str(),sizeof(_domicilio)-1);
+    _domicilio[sizeof(_domicilio)-1]= '\0';
+}
+void Persona::setMail(const string &mail){
+    strncpy(_mail,mail.c_str(),sizeof(_mail)-1);
+    _mail[sizeof(_mail)-1]= '\0';
+}
+void Persona::setTelefono(const string &telefono){
+    strncpy(_telefono,telefono.c_str(),sizeof(_telefono)-1);
+    _telefono[sizeof(_telefono)-1]= '\0';
+}
+void Persona::setSexo(const string &sexo){
+    strncpy(_sexo,sexo.c_str(),sizeof(_sexo)-1);
+    _sexo[sizeof(_sexo)-1]= '\0';
+}
+void Persona::setFechaNac (Fecha f){
+    fechaNac=f;
+}
+void Persona::setFechaIngreso(Fecha f){
+    fechaIngreso=f;
+}
+void Persona::setEstado(bool estado){
+    _estado=estado;
+}
+
+
+
 
--- Persona.h
+++ Persona.h
@@ -1,11 +1,12 @@
 #pragma once
-
+#include <string>
+#include "Fecha.h"
 
 class Persona
 {
     public:
         Persona();
-        virtual ~Persona();
+
         void cargar();
         void mostrar();
         bool escribir();
@@ -24,13 +25,13 @@ class Persona
         bool getEstado();
         int getEdad();
         int getAntiguedad();
-        void setDNI(std::string dni);
-        void setNombre(std::string nombre);
-        void setApellido(std::string apellido);
-        void setDomicilio(std::string domicilio);
-        void setMail(std::string mail);
-        void setTelefono(std::string telefono);
-        void setSexo(std::string sexo);
+        void setDNI(const std::string &dni);
+        void setNombre(const std::string &nombre);
+        void setApellido(const std::string &apellido);
+        void setDomicilio(const std::string &domicilio);
+        void setMail(const std::string &mail);
+        void setTelefono(const std::string &telefono);
+        void setSexo(const std::string &sexo);
         void setFechaNac (Fecha);
         void setFechaIngreso(Fecha);
         void setEstado(bool);
@@ -39,10 +40,10 @@ class Persona
     protected:
 
     private:
-       int idPersona;
+       int _idPersona;
        char _DNI [10], _nombre[20], _apellido[20], _domicilio[30], _mail[30], _telefono[14], _sexo[2];
        Fecha fechaNac, fechaIngreso;
-       bool estado;
+       bool _estado;
 };
 
 
